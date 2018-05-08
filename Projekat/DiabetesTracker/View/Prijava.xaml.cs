9ddc2744d704dca426fe7c57d65092aec6012509
zramic1 <zerina.ramic@live.com>
2018-05-08 15:39:48 +0200

    Izvrsene neke promjene nad formama

---
+++ Projekat/DiabetesTracker/View/Prijava.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.ViewManagement;
+using Windows.UI;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DiabetesTracker
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+       
+        public MainPage()
+        {
+            this.InitializeComponent();
+           
+
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            setTitleBarBackground();
+        }
+
+        private void setTitleBarBackground()
+        {
+            //Instanca Title Bar-a
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            //postavi boju
+            titleBar.BackgroundColor = Color.FromArgb(0, 97, 137, 197);
+
+            //postavi boju dugmadi
+            titleBar.ButtonBackgroundColor = Color.FromArgb(0,97,137,197);
+
+            //promjena naziva prozora (TitleBar)
+            ApplicationView appView = ApplicationView.GetForCurrentView();
+            appView.Title = "Prijava";
+        }
+
+        //iako se nakon pritska na dugme prijava ne prelazi na forme za registraciju, ovu funkcionalnost smo implementirali samo kako bi se sve forme mogle prelistati nakon pokretanja UWP-a
+        private void Prijava_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(BlankPage1));    
+        }
+    }
+}
---
+++ Projekat/DiabetesTracker/View/Registracija2.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace DiabetesTracker
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Registracija2 : Page
+    {
+        List<string> tipDijabetesa = new List<string>();
+        List<string> fizickaAktivnost = new List<string>();
+
+        public Registracija2()
+        {
+            this.InitializeComponent();
+
+            tipDijabetesa.Add("Tip 1");
+            tipDijabetesa.Add("Tip 2");
+            tipDijabetesa.Add("Gestacijski dijabetes");
+
+            fizickaAktivnost.Add("Malo ili nimalo");
+            fizickaAktivnost.Add("1-3 dana sedmično");
+            fizickaAktivnost.Add("3-5 dana sedmično");
+            fizickaAktivnost.Add("6-7 dana sedmično");
+
+            setTitleBarBackground();
+        }
+
+        private void setTitleBarBackground()
+        {
+            //Instanca Title Bar-a
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            //postavi boju
+            titleBar.BackgroundColor = Color.FromArgb(0, 97, 137, 197);
+
+            //postavi boju dugmadi
+            titleBar.ButtonBackgroundColor = Color.FromArgb(0, 97, 137, 197);
+
+            //promjena naziva prozora (TitleBar)
+            ApplicationView appView = ApplicationView.GetForCurrentView();
+            appView.Title = "Registracija";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Terapija));
+        }
+    }
+}
